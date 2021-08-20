c8336f2e1ea691385c678785725e59fc40b15a2e
dinakar29 <26552821+dinakar29@users.noreply.github.com>
2021-08-20 10:50:17 -0500

    Update dockerfile and entrypoint

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,8 @@ FROM rocker/verse:4.1.0
 COPY report-generator.R /main.R
 COPY template.Rmd /template.Rmd
 
+# Set exec permissions on entrypoint script
+RUN chmod +x /main.R
+
 # Set entrypoint
 ENTRYPOINT ["/main.R"]
--- report-generator.R
+++ report-generator.R
@@ -6,7 +6,13 @@ template_path <- Sys.getenv("INPUT_TEMPLATE_PATH")
 report_type <- Sys.getenv("INPUT_REPORT_FORMAT")
 report_dir <- Sys.getenv("INPUT_REPORT_DIR")
 
+# Set report file path
+report_file_path <- file.path(report_dir, paste0("validation-report.", report_type))
+
 # Navigate to package root
 setwd(pkg_root)
 
 # TODO: Implement additional logic
+
+# Placeholder till TODO above is complete
+file.create(report_file_path)
