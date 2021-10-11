c8238964c4e820b7456ffa2001cbb1d67b8d4aca
dinakar29 <26552821+dinakar29@users.noreply.github.com>
2021-10-11 17:43:05 -0500

    KISS

--- dependencies.R
+++ dependencies.R
@@ -11,40 +11,5 @@ github_packages <- c(
 lapply(github_packages, remotes::install_github)
 
 # Install TinyTex globally
-install_tinytex_globally <- function() {
-    tinytex_installer <- '
-wget -qO- \
-    "https://raw.githubusercontent.com/yihui/tinytex/master/tools/install-unx.sh" \
-    | sh -s - --admin --no-path
-mv ~/.TinyTeX /opt/TinyTeX
-/opt/TinyTeX/bin/*/tlmgr path add
-tlmgr install \
-    makeindex \
-    metafont \
-    mfware \
-    inconsolata \
-    tex \
-    ae \
-    parskip \
-    listings \
-    xcolor \
-    epstopdf-pkg \
-    pdftexcmds \
-    kvoptions \
-    texlive-scripts \
-    grfext
-tlmgr path add
-'
-
-    system(tinytex_installer)
-    tinytex::r_texmf()
-
-    permission_update <- '
-chown -R root:staff /opt/TinyTeX
-chmod -R g+w /opt/TinyTeX
-chmod -R g+wx /opt/TinyTeX/bin
-export PATH=/opt/TinyTeX/bin:${PATH}
-echo "PATH=${PATH}" >> ${R_HOME}/etc/Renviron
-'
-    system(permission_update)
-}
+tinytex::install_tinytex()
+system("mv $HOME/bin/* /usr/local/bin")
