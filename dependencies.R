6835d835c80c15440e19c86ff5707d3e50e6363c
knightdave <13086223+knightdave@users.noreply.github.com>
2021-11-24 14:07:49 +0000

    Update action to use tinytex installation

--- dependencies.R
+++ dependencies.R
@@ -11,7 +11,33 @@ github_packages <- c(
 lapply(github_packages, remotes::install_github)
 
 # CRAN
-install.packages("kableExtra")
+options(repos = c("https://cloud.r-project.org/"))
+ncores <- parallel::detectCores(all.tests = FALSE, logical = TRUE)
+if (!require("kableExtra")) install.packages("kableExtra", upgrade = "never", Ncpus = ncores)
+if (!require("tinytex")) install.packages("tinytex", upgrade = "never", Ncpus = ncores)
 
-# Install missing fonts
-system("tlmgr install courier ec")
+# Conditionally install TinyTex
+if(!dir.exists("/__w/_temp/TinyTeX")) {
+# nolint start
+  tinytex_installer <- '
+export TINYTEX_DIR=/__w/_temp/TinyTeX
+wget -qO- "https://raw.githubusercontent.com/yihui/tinytex/master/tools/install-unx.sh" | sh -s - --admin --no-path
+mkdir -p /__w/_temp/TinyTeX
+cp -r ~/.TinyTeX/. /__w/_temp/TinyTeX
+rm -rf ~/.TinyTeX
+/__w/_temp/TinyTeX/bin/*/tlmgr path add
+tlmgr install latex-bin luatex xetex ae bibtex context inconsolata listings makeindex metafont mfware parskip pdfcrop tex tools url xkeyval courier ec
+tlmgr path add
+'
+  # nolint end
+  system(tinytex_installer)
+  tinytex::r_texmf()
+  permission_update <- '
+chown -R root:staff /__w/_temp/TinyTeX
+chmod -R g+w /__w/_temp/TinyTeX
+chmod -R g+wx /__w/_temp/TinyTeX/bin
+export PATH=/__w/_temp/TinyTeX/bin:${PATH}
+echo "PATH=${PATH}" >> $GITHUB_ENV
+'
+  system(permission_update)
+}
