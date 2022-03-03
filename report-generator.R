824c7d82b830259f3eaec2fdb750e0e78e42010b
Dinakar <26552821+dinakar29@users.noreply.github.com>
2022-03-03 14:30:49 -0600

    Feature: Select a prefix for the validation report (#55)
    * tlmgr fails if it is not updated on runtime
    
    * Some more enhancements
    
    * Couple 'o bugfixes
    
    * Composite actions are silly
    
    * Remove docker references
    
    * Get basename of output file

--- report-generator.R
+++ report-generator.R
@@ -7,10 +7,14 @@ cat(sprintf("Current dir is: '%s'", getwd()))
 pkg_dir <- normalizePath(Sys.getenv("INPUT_REPORT_PKG_DIR", "."))
 template_path <- Sys.getenv("INPUT_REPORT_TEMPLATE_PATH", "/template.Rmd")
 report_format <- Sys.getenv("INPUT_REPORT_RMARKDOWN_FORMAT", "all")
-disable_install_dev_deps <- tolower(Sys.getenv("DISABLE_INSTALL_DEV_DEPS")) %in% c('yes', 'y', 't', 'true')
+report_output_prefix <- Sys.getenv("INPUT_REPORT_OUTPUT_PREFIX", "")
+disable_install_dev_deps <- tolower(
+  Sys.getenv("DISABLE_INSTALL_DEV_DEPS")
+) %in% c("yes", "y", "t", "true")
 
 # fail with meaningful message if REPORT_PKG_DIR does not appear to be a package
-if (!file.exists(file.path(pkg_dir, "DESCRIPTION"))) {
+desc_file <- file.path(pkg_dir, "DESCRIPTION")
+if (!file.exists(desc_file)) {
   stop(sprintf(
     paste(sep = "\n",
       "Could not find package at '%s'",
@@ -24,7 +28,7 @@ if (!file.exists(file.path(pkg_dir, "DESCRIPTION"))) {
 }
 
 # Install package dependencies
-if (!disable_install_dev_deps){
+if (!disable_install_dev_deps) {
     options("remotes.git_credentials" = git2r::cred_user_pass(
         username = "token",
         password = remotes:::github_pat()
@@ -32,13 +36,26 @@ if (!disable_install_dev_deps){
     devtools::install_dev_deps(pkg_dir, upgrade = "never")
 }
 
+# Set the output file name
+if (report_output_prefix == "") {
+  desc <- read.dcf(desc_file)
+  pkg_name <- toString(desc[, "Package"])
+  pkg_version <- toString(desc[, "Version"])
+  report_output_prefix <- paste0(
+    pkg_name, "-", pkg_version, "-validation-report"
+  )
+}
+
 # allow rmarkdown to choose appropriate file extension for output format
 report_file_path <- rmarkdown::render(
   template_path,
   output_dir = getwd(),  # create report wherever R script was called
-  output_file = "validation_report",
+  output_file = report_output_prefix,
   output_format = report_format,
   params = list(pkg_dir = pkg_dir)
 )
 
-cat(sprintf("Created report at: '%s'", report_file_path))
+# Create a tmp file which contains the final report filename
+writeLines(report_file_path, "/tmp/report_file_path.txt")
+
+cat(sprintf("Created report at: '%s'\n\n", report_file_path))
