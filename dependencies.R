d28578a9fe1e2a3a09e6d185aea259e5b0109a87
Franciszek Walkowiak <franciszek.walkowiak@roche.com>
2024-05-07 15:09:30 +0200

    Don't use knitr and kableExtra

--- dependencies.R
+++ dependencies.R
@@ -14,4 +14,4 @@ lapply(github_packages, remotes::install_github)
 options(repos = c("https://cloud.r-project.org/"))
 ncores <- parallel::detectCores(all.tests = FALSE, logical = TRUE)
 if (!require("git2r")) install.packages("git2r", upgrade = "never", Ncpus = ncores)
-if (!require("kableExtra")) install.packages("kableExtra", upgrade = "never", Ncpus = ncores)
+
